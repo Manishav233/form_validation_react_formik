ae85e54b1f0e35814d244e336d982bac7cf6beab
Manishav233 <manishav233@gmail.com>
2022-06-08 14:52:44 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import {Formik} from 'formik';
 import './App.css';
+import ContactForm from './components/ContactForm';
+import * as Yup from "yup";
 
 function App() {
+  const ErrorSchema = Yup.object().shape({
+    username: Yup.string()
+      .required("Required")
+      .min(2, "too Short")
+      .max(12, "Too Long"),
+      
+      age: Yup.number()
+      .typeError("Age must be a number")
+      .integer("must be an integer")
+      .required("Required")
+      .min(18, "You must be at least 18 years")
+      .max(60, "You must be at less than 60 years"),
+      
+    email: Yup.string().email("Invalid email").required("Required"),
+    
+    phone: Yup.string()
+      .matches(
+        /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/,
+        "Not a Phone number"
+      )
+      .required("Required"),
+      
+    message: Yup.string()
+      .min(5, "Too Short!")
+      .max(100, "Too Long!")
+      .required("Required"),
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container pt-5">
+    <div className="row justify-content-sm-center pt-5">
+      <div className="col-sm-6 shadow round pb-3">
+        <h1 className="text-center pt-3 text-secondary">Contact Form</h1>
+  
+        <Formik
+            initialValues={{
+              username: "",//name of our field 
+              age: "",
+              email: "",
+              phone: "",
+              message: "",
+            }}
+            onSubmit={(value, { resetForm }) => {
+              console.log(value);
+              resetForm();
+            }}
+            validationSchema={ErrorSchema}
+            component={ContactForm}
+          />
+
+    </div>
+    </div>
     </div>
   );
 }
